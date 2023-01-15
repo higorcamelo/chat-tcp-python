5e332f552e47ca26081d3ece1aa23b7757a1fb59
Higor Camelo <higorcamelo@gmail.com>
2023-01-15 00:10:14 -0300

    Update chat.py

--- chat.py
+++ chat.py
@@ -2,30 +2,30 @@ import PySimpleGUI as sg
 
 
 def jn_usuario():
-        layout = [
+    layout = [
         [sg.Text('Insira seu apelido:')],
         [sg.InputText(key = 'usuario')],
         [sg.Button('Ok')]
-        ]
-        return sg.Window('Chat TCP',Layout = layout)
+    ]
+    return sg.Window('Chat TCP',Layout = layout, finalize = True)
 
 def jn_chat():
     layout = [
-        [sg.Text('Olá,'+ jn_usuario(self))]
         [sg.Text('Insira um IP para se conectar:')],
         [sg.InputText(key = 'ip')],
         [sg.Output(size = (30,60))],
         [sg.InputText(key = 'mensagem'), sg.Button(Enviar)]
     ]
-    return sg.Window('Chat TCP', Layout = layout)
+    return sg.Window('Chat TCP', Layout = layout, finalize = True)
 
-def Iniciar(self):
-    while True:
-        self.evento, self.values = self.criarJanela.Read()
-        if( self.evento == sg.WIN_CLOSED):
-            break
-        elif(self.evento == 'Ok'):
-            nome_usuario = self.values['usuario']
-            jn_chat()
-            print(nome_usuario)
+
+janela_usuario, janela_chat = jn_usuario(), None
+while True:
+    window, event, values = sg.read_all_windows()
+    
+    if window == janela_usuario and event == sg.WIN_CLOSED:
+        break
+    if window == janela_usuario and event == 'Ok':
+        janela_chat = jn_chat()
+        janela_usuario.hide()
 
