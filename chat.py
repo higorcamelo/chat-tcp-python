1f3b99e73ec29c57d22750fa908ba8da9671dee5
Higor Camelo <higorcamelo@gmail.com>
2023-01-16 20:25:27 -0300

    Update chat.py

--- chat.py
+++ chat.py
@@ -7,16 +7,17 @@ def jn_usuario():
         [sg.InputText(key = 'usuario')],
         [sg.Button('Ok')]
     ]
-    return sg.Window('Chat TCP',Layout = layout, finalize = True)
+    return sg.Window('Chat TCP', layout, finalize = True)
 
-def jn_chat():
+def jn_chat(nome_usuario):
     layout = [
+        [sg.Text('Olá, ' + nome_usuario)]
         [sg.Text('Insira um IP para se conectar:')],
         [sg.InputText(key = 'ip')],
         [sg.Output(size = (30,60))],
         [sg.InputText(key = 'mensagem'), sg.Button(Enviar)]
     ]
-    return sg.Window('Chat TCP', Layout = layout, finalize = True)
+    return sg.Window('Chat TCP', layout, finalize = True)
 
 
 janela_usuario, janela_chat = jn_usuario(), None
@@ -26,6 +27,6 @@ while True:
     if window == janela_usuario and event == sg.WIN_CLOSED:
         break
     if window == janela_usuario and event == 'Ok':
-        janela_chat = jn_chat()
+        janela_chat = jn_chat(values['usuario'])
         janela_usuario.hide()
 
