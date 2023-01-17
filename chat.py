56341339f11458e329f0f16917d2db54bb7e3dd4
Higor Camelo <higorcamelo@gmail.com>
2023-01-17 17:40:57 -0300

    Avanço na GUI

--- chat.py
+++ chat.py
@@ -11,22 +11,29 @@ def jn_usuario():
 
 def jn_chat(nome_usuario):
     layout = [
-        [sg.Text('Olá, ' + nome_usuario)]
-        [sg.Text('Insira um IP para se conectar:')],
+        [sg.Text('Olá, ' + nome_usuario)],
+        [sg.Text('Insira um IP para se conectar:'), sg.Button('Conectar')],
         [sg.InputText(key = 'ip')],
-        [sg.Output(size = (30,60))],
-        [sg.InputText(key = 'mensagem'), sg.Button(Enviar)]
+        [sg.Output(size = (60,20))],
+        [sg.InputText(key = 'mensagem'), sg.Button('Enviar')],
     ]
     return sg.Window('Chat TCP', layout, finalize = True)
 
+def main():
+    janela_usuario, janela_chat = jn_usuario(), None
+    while True:
+        window, event, values = sg.read_all_windows()
+        
+        if event == sg.WIN_CLOSED:
+            break
+        if window == janela_usuario and event == 'Ok':
+            if(values['usuario'] == ''):
+                sg.Popup('Insira um apelido válido')
+            else:   
+                janela_chat = jn_chat(values['usuario'])
+                janela_usuario.hide()
+            
 
-janela_usuario, janela_chat = jn_usuario(), None
-while True:
-    window, event, values = sg.read_all_windows()
-    
-    if window == janela_usuario and event == sg.WIN_CLOSED:
-        break
-    if window == janela_usuario and event == 'Ok':
-        janela_chat = jn_chat(values['usuario'])
-        janela_usuario.hide()
 
+if __name__ == '__main__':
+    main()
