e742d953e18f4ffa14c1c35828687b75065ddceb
Higor Camelo <higorcamelo@gmail.com>
2023-01-22 20:58:58 -0300

    Update conexao.py

--- conexao.py
+++ conexao.py
@@ -5,9 +5,13 @@ servidor.bind(('localhost',8888))
 
 servidor.listen()
 
+nome_usuario = ''
 cliente, end = servidor.accept()
 finalizado = False
 
 while not finalizado:
     msg = cliente.recv(1024).decode('UTF-8')
-    
+    print(nome_usuario + ' ' + msg)
+
+cliente.close()
+servidor.close()
