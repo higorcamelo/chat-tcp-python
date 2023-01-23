baf96d2d459b71eb936e94378168ee21a2f7411f
Higor Camelo <higorcamelo@gmail.com>
2023-01-23 20:08:04 -0300

    Arquivo servidor sendo refeito

--- servidor.py
+++ servidor.py
@@ -1,17 +1,21 @@
 import socket
+import threading
+
+
+host = ''
 
 servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-servidor.bind(('localhost',8888))
+servidor.bind((host, 8888))
 
 servidor.listen()
 
-nome_usuario = ''
+apelido, clientes = []
 cliente, end = servidor.accept()
 finalizado = False
 
 while not finalizado:
     msg = cliente.recv(1024).decode('UTF-8')
-    print(nome_usuario + ' ' + msg)
+    print(apelido + ' ' + msg)
 
 cliente.close()
 servidor.close()
